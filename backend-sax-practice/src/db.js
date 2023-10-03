41d6264e574f9bd91aa4fba1ebdec5afddf3fa44
Bard of Code <harnettmusic@gmail.com>
2023-10-03 11:16:30 -0230

    updating

--- backend-sax-practice/src/db.js
+++ backend-sax-practice/src/db.js
@@ -1,12 +1,8 @@
 import { MongoClient } from "mongodb";
+import "dotenv/config";
 
 let db;
-// Old uri connection. Maybe no atlas project attached.
-// const uri = 'mongodb+srv://saxDev:dRT0ZLfvGahCLmWf@cluster0.owezgre.mongodb.net/'
 
-// Atlas access
-// let user = "dev";
-// let pass = "mBqMsEyHkP2qxv9Z";
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.2sjlgxi.mongodb.net/?retryWrites=true&w=majority`;
 
 async function connectToDb(cb) {
