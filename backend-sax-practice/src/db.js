9051430cc2b06a91ce9e962e7f098df9974ec0b6
Bard of Code <harnettmusic@gmail.com>
2023-10-03 10:33:02 -0230

    initial

--- backend-sax-practice/src/db.js
+++ backend-sax-practice/src/db.js
@@ -1,17 +1,20 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
 let db;
-const uri = 'mongodb+srv://saxDev:dRT0ZLfvGahCLmWf@cluster0.owezgre.mongodb.net/'
+// Old uri connection. Maybe no atlas project attached.
+// const uri = 'mongodb+srv://saxDev:dRT0ZLfvGahCLmWf@cluster0.owezgre.mongodb.net/'
+
+// Atlas access
+// let user = "dev";
+// let pass = "mBqMsEyHkP2qxv9Z";
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.2sjlgxi.mongodb.net/?retryWrites=true&w=majority`;
 
 async function connectToDb(cb) {
-    const client = new MongoClient(uri)
-    await client.connect();
-    //set name of database
-    db = client.db('TheShed');
-    cb()
+  const client = new MongoClient(uri);
+  await client.connect();
+  //set name of database
+  db = client.db("TheShed");
+  cb();
 }
 
-export {
-    db,
-    connectToDb
-};
+export { db, connectToDb };
--- backend-sax-practice/src/server.js
+++ backend-sax-practice/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { db, connectToDb } from "./db.js";
+import "dotenv/config";
 import cors from "cors";
 
 const app = express();
