218f981bb6b5f9d1251af3935f7ca75630eef352
Bard of Code <harnettmusic@gmail.com>
2023-10-13 13:34:19 -0230

    fix http

--- backend-sax-practice/src/server.js
+++ backend-sax-practice/src/server.js
@@ -200,10 +200,6 @@ const httpsServer = https.createServer(
   app
 );
 
-httpServer.listen(80, () => {
-  console.log("HTTP Server running on port 80");
-});
-
 httpsServer.listen(443, () => {
   console.log("HTTPS Server running on port 443");
 });
